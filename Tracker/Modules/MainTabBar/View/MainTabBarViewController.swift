4d5b2d4f7ba8d20aa39e77544f5e9b78bd3d1c88
Matvei Plokhov <mpplokho@gmail.com>
2025-06-04 18:06:00 +0300

    Changed project structure

--- Tracker/Modules/MainTabBar/View/MainTabBarViewController.swift
+++ Tracker/Modules/MainTabBar/View/MainTabBarViewController.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-final class MainTabBarController: UITabBarController {
+final class MainTabBarViewController: UITabBarController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
